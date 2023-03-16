4e1671ab15b09908382ef6ec75061579f39981ea
galsamy <galsamytl@gmail.com>
2023-03-16 16:23:40 +0200

    started working on profile page

---
+++ public/scripts/profile.js
@@ -0,0 +1,11 @@
+const sells_li = $("#sells")
+const orders_li = $("#orders")
+sells_li[0].addEventListener("click", ()=>{
+    console.log("sells")
+})
+
+orders_li[0].addEventListener("click", ()=>{
+    console.log("orders")
+
+})
+
