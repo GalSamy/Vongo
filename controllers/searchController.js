9dc7d5c106176a9b7bc1f9c026cf51ea84795581
GalSamy <59654405+GalSamy@users.noreply.github.com>
2023-03-23 21:40:56 +0200

    created listing page with api connection

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,13 +1,26 @@
 const {Listings} = require("../models/listingModel")
-
- search = async (req,res) => {
+const search = async (req,res) => {
     const listings =await Listings.find({})
     res.render('../views/listings.ejs', {Items:{listings}});
 }
-const listing = (req,res) => {
+const listing =async (req,res) => {
     const listingId = req.params.id
-   // item = items.get(listingId)
-    res.render('../views/listing.ejs', {Item:{listingId}});
+    const l = await Listings.findOne({_id: listingId}).catch(() => {
+        res.sendStatus(404)
+    })
+    const name = l.name.replace(/ /g,"%20")
+    let Album = await fetch("https://api.deezer.com/search/album/?q=The%20Dark%20Side%20Of%20The%20Moon") // change listing scheme to include album Id of api!!
+    Album = await Album.json()
+    let Songs = await fetch(Album.data[0].tracklist)
+    Songs = await Songs.json()
+    let Genre = await fetch("https://api.deezer.com/genre/" + Album.data[0].genre_id)
+    Genre = await Genre.json()
+    res.render('../views/listing.ejs', {
+        Item:l,
+        Album: Album.data[0],
+        Songs: Songs.data,
+        Genre: Genre
+    });
 }
 
 const newListing = (req,res)=>{
