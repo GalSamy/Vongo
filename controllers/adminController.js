a68b0ef6120c9c6915db589247b0403175f77b88
tal4 <ykvnkl2@gmail.com>
2023-03-29 15:10:17 +0300

    admin router

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const {Users} = require("../models/userModel")
-
+const {listingModel,Listings} = require("../models/listingModel")
 const admin = async (req,res)=>{
-    let sells_data = await Users.find({Sells:{ $exists: true, $not: { $size: 0 } } }, {Sells: 1, _id: 0})
-    console.log(sells_data)
+    let sells_data = await Listings.find({closed:true })
+    console.log("sells data"+sells_data[0]['_id'])
+    console.log("sells data"+sells_data)
     res.render("../views/adminPanel.ejs",{User:res.locals, isProfile:true})
 }
 module.exports={admin}
