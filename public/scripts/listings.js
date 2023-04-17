82f0fd6d6fc2c0f96ab23816a08c20be61869fc2
galsamy <galsamytl@gmail.com>
2023-04-17 21:26:58 +0300

    started working on listing search

--- public/scripts/listings.js
+++ public/scripts/listings.js
@@ -4,31 +4,7 @@ window.addEventListener("load", (event) => {
     socket.emit("joinListing")
     console.log(socket.connected)
   });
-/*function handleBid(listingId, bidBy){
-    let bidAmount = $("#bidAmount")[0].value
-    console.log(bidBy)
-    if (!bidAmount)
-        return alert("put a bid")
-    $.ajax(
-        {
-        url: "http://localhost:8080/utils/newBid/",
-        type: 'Post',
-        data:{
-        bid:bidAmount,
-        listing: listingId,
-        bidBy: bidBy
-        },
-        contentType:"application/x-www-form-urlencoded",
-        dataType:"json",
-        success:async  function(response) {
-            console.log("response:" + response.lastBid)
-            $("#lastbid")[0].textContent ="Last Bid : " + response.lastBid + "$"
-        },
-        error: function(xhr, status, error) {
-            console.log(xhr.responseText); // Handle error response
-        }
-        })
-}*/
+
 function refreshPage() {
     $.ajax({
       url: '/listings',
@@ -54,3 +30,16 @@ socket.on('newListing',()=>{
     console.log('newListing sent!')
     refreshPage()
 })
+
+const NameCheck = $("#NameParameter")[0]
+const ArtistCheck = $("#ArtistParameter")[0]
+const MinimumCheck = $("#MinPriceParameter")[0]
+const MaximumCheck = $("#MaxPriceParameter")[0]
+const YearParameter = $("#YearParameter")[0]
+
+const NameField = $("#NameField")[0]
+const ArtistField = $("#ArtistField")[0]
+const MinimumField = $("#MinPriceField")[0]
+const MaximumField = $("#MaxPriceField")[0]
+const YearField = $("#YearField")[0]
+
